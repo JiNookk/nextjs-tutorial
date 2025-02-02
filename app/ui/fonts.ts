7fdacfbe7f27c19e339a2475cb18d8858fe1bca6
liam-classum <liam@classum.com>
2025-02-02 16:18:14 +0900

    add: projects setting

---
+++ app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import {Inter, Lusitana} from 'next/font/google'
+import {undefined} from "zod";
+
+export const inter = Inter({subsets: [
+    'latin'
+        // 'greek'
+        // 'vietnamese'
+    ]})
+
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+})
+
+
+
